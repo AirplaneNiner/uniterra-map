786c6ec6478a12c59c05fc356fe772e7771e419a
AirplaneNiner <91838121+AirplaneNiner@users.noreply.github.com>
2025-05-11 20:17:04 +0200

    Update map.js

--- src/map.js
+++ src/map.js
@@ -18,7 +18,7 @@ class CustomTileLayer extends L.TileLayer {
       zzz += "z";
     }
     if (zzz.length != 0) zzz += "_";
-    const url = `https://142.4.216.95:8192/main/tiles/tashret/flat/${group.x}_${group.y}/${zzz}${numberInGroup.x}_${numberInGroup.y}.png`;
+    const url = `https://142.4.216.95:8192/tashret/tiles/new/flat/${group.x}_${group.y}/${zzz}${numberInGroup.x}_${numberInGroup.y}.png`;
     return url;
   }
 }
